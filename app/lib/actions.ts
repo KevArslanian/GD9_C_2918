b089165338af1fbf77fb8fed65e8981a8789d8bf
Delta Oga Nophen Putra <KevArslanian@users.noreply.github.com>
2026-05-28 14:29:37 +0700

    fix: use portable password hashing

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import type { User } from '@/app/lib/definitions';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
